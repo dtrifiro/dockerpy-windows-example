4d021d36ab83f6d4d7f9203fefb3b2f0107e10e0
Daniele Trifirò <daniele@iterative.ai>
2022-08-19 01:32:47 +0200

    test sha256 tag

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
     from urllib.parse import quote
 
     image = "python"
-    tag = "3.8-slim"
+    tag = "sha256:726098870bb14355cb12572764a3be191b42f8b15947d97affe3b8ec6a7a446e"
     pulled = client.images.pull(image, tag=tag)
     print(f"{pulled=}")
     out = run(pulled.tags[0])
